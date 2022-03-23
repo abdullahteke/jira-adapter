7dcd834b56467bbaaf29fa24ac2fb3034a7e53c4
Abdullah TEKE <abdullahteke@gmail.com>
2022-03-23 15:25:53 +0300

    first commit
    first commit

---
+++ src/com/abdullahteke/ysd/JiraAdapter/JiraManager.java
@@ -0,0 +1,157 @@
+package com.abdullahteke.ysd.JiraAdapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JiraManager {
+
+	  private URL jiraURL;
+	  private String userName;
+	  private String userPassword;
+	  private String projectKey;
+	  HttpURLConnection connection;
+	  private String authorizationCode;
+	  
+	  
+
+	private static JiraManager instance;
+
+
+	public static JiraManager getInstance(String jiraURL,String userName,String userPassword, String projectKey) {
+		
+		if (instance==null){
+			instance=new JiraManager( jiraURL, userName, userPassword,  projectKey);
+		}
+		
+		return instance;
+	}
+	
+    public JiraManager(String jiraURL,String userName,String userPassword, String projectKey) {
+    	
+    	this.userName=userName;
+    	this.userPassword=userPassword;
+    	this.projectKey=projectKey;
+    	this.connection=null;
+    	this.authorizationCode=getAuthorizationCode();
+    	
+    	try {
+    		
+			this.jiraURL = new URL(jiraURL+"/rest/api/2/issue");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    	
+	}
+    
+    private String getAuthorizationCode() {
+    	String retVal="Basic ";
+    	 	
+		try {
+			String decodedPass= new String(Base64.getDecoder().decode(userPassword), "UTF-8");
+	    	byte[] encodedPassword = ( userName + ":" + decodedPass).getBytes("UTF-8");
+	    	  
+	    	retVal=retVal+Base64.getEncoder().encodeToString(encodedPassword);
+	    	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	  return retVal;
+	}
+
+	public String  createNewIssue(String id, String title, String description, String severity, String category, String subCategory, String component) {
+    	String retVal="";
+    	String msg=queryMessage(id,title,description,severity,category,subCategory,component);
+    	
+    	try {
+			connection=(HttpURLConnection) jiraURL.openConnection();
+	    	connection.setDoOutput(true);
+	    	connection.setRequestMethod("POST");
+	    	connection.setRequestProperty("Content-Type", "application/json");
+	    	connection.addRequestProperty("Authorization", authorizationCode);
+	    	
+	    	OutputStream os= connection.getOutputStream();
+	    	os.write(msg.getBytes());
+	    	os.flush();
+
+	    	if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+	    		throw new RuntimeException("Failed : HTTP error code : "
+	    				+ connection.getResponseCode()+" : "+ connection.getResponseMessage());
+	    	}
+	    	
+			BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+
+		    InputStream is = connection.getInputStream();
+		    StringBuffer buf = new StringBuffer();
+		    int c;
+		    while( ( c = is.read() ) != -1 ) {
+		      buf.append( (char) c );
+		    }
+			
+		    retVal=buf.toString();
+		    
+		    String regexPattern = ".*\"key\":\"(SKOP.*)\",.*";
+		    Pattern pattern = Pattern.compile(regexPattern);
+		    Matcher matcher=pattern.matcher(buf.toString());
+		       
+		    if (matcher.matches()) {
+		    	retVal=matcher.group(1);
+		    }
+		       
+			connection.disconnect();
+
+	    	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	System.out.println(retVal);
+    	return retVal;
+    	
+    	
+    }
+    
+    
+    public String queryMessage(String id, String title, String description, String severity, String category, String subCategory, String component){
+    	String retVal="{";
+    	retVal=retVal+"\n\t\"fields\":{";
+    	retVal=retVal+"\n\t\t\"project\":";
+    	retVal=retVal+"\n\t\t{";
+    	retVal=retVal+"\n\t\t\t\"key\":\""+projectKey+"\"";
+    	retVal=retVal+"\n\t\t},";
+    	retVal=retVal+"\n\t\t\"summary\":\""+title+"\",";
+    	retVal=retVal+"\n\t\t\"description\":\""+description+"\",";
+    	retVal=retVal+"\n\t\t\"issuetype\":";
+    	retVal=retVal+"\n\t\t{";
+    	retVal=retVal+"\n\t\t\t\"name\":\"Task\"";
+    	retVal=retVal+"\n\t\t},";
+    	retVal=retVal+"\n\t\t\"components\":";
+    	retVal=retVal+"\n\t\t[{";
+    	retVal=retVal+"\n\t\t\t\"name\":\""+component+"\"";
+    	retVal=retVal+"\n\t\t}],";
+    	retVal=retVal+"\n\t\t\"priority\":";
+    	retVal=retVal+"\n\t\t{";
+    	retVal=retVal+"\n\t\t\t\"name\":\"Critical\"";
+    	retVal=retVal+"\n\t\t}";
+    	retVal=retVal+"\n\t}";
+    	retVal=retVal+"\n}";
+    	
+    	System.out.println(retVal);
+    	return retVal;
+    }
+    
+	
+}
---
+++ src/main/JiraAdapter.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+import com.abdullahteke.ysd.JiraAdapter.JiraManager;
+
+public class JiraAdapter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		  String jiraURL="https://jiraurl.com.tr";
+		  String userName="jira_user";
+		  String userPassword="jira_password";
+		  String projectKey="project_key";
+		  
+		String id=""; 
+		String title="Deneme : Mersin"; 
+		String description="Mersin deneme";
+		String severity="Critical";
+		String category="category"; 
+		String subCategory="asdfasdf";
+		String components="VeriTabani_KritikAlarm_Sorumlusu";
+		
+		String  password= "0719SK_Bilkent_OpsBridge";
+		
+		String encoded= Base64.getEncoder().encodeToString(password.getBytes());
+		System.out.println(encoded);
+		
+		String decodedPass = null;
+		try {
+			decodedPass = new String(Base64.getDecoder().decode(userPassword), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(decodedPass);
+		
+		
+	String ticketNumber=JiraManager
+			.getInstance(jiraURL, userName, userPassword, projectKey)
+			.createNewIssue(id,title,description,severity,category,subCategory,components);
+	
+	System.out.println(ticketNumber+ " is opened!");
+
+   
+	}
+    
+}
